0099e35ecefa87a453c48f7045fcb30740467268
yuhy <361869521@qq.com>
2015-11-12 00:31:31 +0800

    add gallery image margin

--- app/src/main/java/com/hengye/share/adapter/RecyclerViewTopicAdapter.java
+++ app/src/main/java/com/hengye/share/adapter/RecyclerViewTopicAdapter.java
@@ -1,16 +1,12 @@
 package com.hengye.share.adapter;
 
 import android.content.Context;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.text.Html;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridLayout;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.volley.view.NetworkImageView;
@@ -18,7 +14,7 @@ import com.hengye.share.R;
 import com.hengye.share.module.Topic;
 import com.hengye.share.util.CommonUtil;
 import com.hengye.share.util.DateUtil;
-import com.hengye.share.util.L;
+import com.hengye.share.util.ViewUtil;
 import com.hengye.volleyplus.toolbox.RequestManager;
 
 import java.util.List;
@@ -27,12 +23,14 @@ public class RecyclerViewTopicAdapter extends RecyclerViewSimpleAdapter<Topic, R
 
     private int mScreenWidth;
     private int mImageViewMaxWidth;
+    private int mImageViewMargin;
     public RecyclerViewTopicAdapter(Context context, List<Topic> datas){
         super(context, datas);
 
         mScreenWidth = context.getResources().getDisplayMetrics().widthPixels;
         int marginHorizontal = context.getResources().getDimensionPixelSize(R.dimen.activity_horizontal_margin) * 2;
         mImageViewMaxWidth = mScreenWidth - marginHorizontal;
+        mImageViewMargin = context.getResources().getDimensionPixelSize(R.dimen.topic_gallery_iv_margin);
     }
 
     @Override
@@ -54,16 +52,13 @@ public class RecyclerViewTopicAdapter extends RecyclerViewSimpleAdapter<Topic, R
         holder.mAvator.setImageUrl(topic.getAvator(), RequestManager.getImageLoader());
 
         if(!CommonUtil.isEmptyList(topic.getImageUrls())){
-            L.debug("find topic urls, position : {}, first url : {}", position, topic.getImageUrls().get(0));
-
-            int imageWidth = getImageWidth(topic.getImageUrls().size());
 
             holder.mGallery.removeAllViews();
-            for(String url : topic.getImageUrls()){
+            holder.mGallery.setColumnCount(ViewUtil.getTopicImageColumnCount(topic.getImageUrls().size()));
+            for(int i = 0; i < topic.getImageUrls().size(); i++){
+                String url = topic.getImageUrls().get(i);
                 NetworkImageView iv = new NetworkImageView(mContext);
-                iv.setScaleType(ImageView.ScaleType.CENTER_CROP);
-                ViewGroup.LayoutParams lp = new ViewGroup.LayoutParams(imageWidth, imageWidth);
-                iv.setLayoutParams(lp);
+                ViewUtil.setTopicImageViewLayoutParams(iv, mImageViewMaxWidth, mImageViewMargin, i, topic.getImageUrls().size());
                 iv.setImageUrl(url, RequestManager.getImageLoader());
                 holder.mGallery.addView(iv);
             }
@@ -92,15 +87,15 @@ public class RecyclerViewTopicAdapter extends RecyclerViewSimpleAdapter<Topic, R
         }
     }
 
-    private int getImageWidth(int size){
-        if(size == 1){
-            return ViewGroup.LayoutParams.WRAP_CONTENT;
-        }else if(size == 2 || size == 4){
-            return mImageViewMaxWidth / 2;
-        }else{
-            return mImageViewMaxWidth / 3;
-        }
-    }
+//    private int getImageWidth(int size){
+//        if(size == 1){
+//            return ViewGroup.LayoutParams.WRAP_CONTENT;
+//        }else if(size == 2 || size == 4){
+//            return mImageViewMaxWidth / 2;
+//        }else{
+//            return mImageViewMaxWidth / 3;
+//        }
+//    }
 }
 
 
---
+++ app/src/main/java/com/hengye/share/util/ViewUtil.java
@@ -0,0 +1,53 @@
+package com.hengye.share.util;
+
+import android.view.ViewGroup;
+import android.widget.GridLayout;
+import android.widget.ImageView;
+
+public class ViewUtil {
+
+    public static int getTopicImageWidth(int maxWidth, int margin, int size){
+        int marginLength = margin * 2;
+        if(size == 1){
+//            return ViewGroup.LayoutParams.WRAP_CONTENT;
+            return (maxWidth - marginLength) / 2;
+        }else if(size == 2 || size == 4){
+            return (maxWidth - marginLength) / 2;
+        }else{
+            return (maxWidth - marginLength) / 3;
+        }
+    }
+
+    public static int getTopicImageColumnCount(int size){
+        if(size == 1){
+            return 1;
+        }else if(size == 2 || size == 4){
+            return 2;
+        }else{
+            return 3;
+        }
+    }
+
+    public static void setTopicImageViewLayoutParams(ImageView iv, int maxWidth, int margin, int position, int size){
+        int imageWidth = getTopicImageWidth(maxWidth, margin, size);
+
+
+        iv.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        GridLayout.LayoutParams lp = new GridLayout.LayoutParams();
+        lp.width = imageWidth;
+        lp.height = imageWidth;
+        lp.rightMargin = margin;
+        lp.bottomMargin = margin;
+        lp.isMarginRelative();
+//        ViewGroup.MarginLayoutParams marginLayoutParams = (ViewGroup.MarginLayoutParams) lp;
+        if(size == 1){
+            iv.setMaxHeight((int)(maxWidth / 2 * 1.5));
+        }
+        iv.setLayoutParams(lp);
+    }
+
+    public static ViewGroup.LayoutParams getTopicImageViewLayoutParams(int imageWidth, int position, int size){
+        ViewGroup.LayoutParams lp = new ViewGroup.LayoutParams(imageWidth, imageWidth);
+        return lp;
+    }
+}
