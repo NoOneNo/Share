c389a7bf128941f2710b991315012966f42678e3
yuhy <361869521@qq.com>
2016-04-07 09:49:20 +0800

    fix Toast util and alter gradle level

--- app/src/main/java/com/hengye/share/util/ToastUtil.java
+++ app/src/main/java/com/hengye/share/util/ToastUtil.java
@@ -10,11 +10,19 @@ import com.hengye.share.ui.base.BaseApplication;
 public class ToastUtil extends ApplicationUtil{
 
     public static void showToast(CharSequence text){
-        Toast.makeText(getContext(), text, Toast.LENGTH_SHORT).show();
+        Toast toast = getToast();
+        toast.setText(text);
+        toast.setDuration(Toast.LENGTH_SHORT);
+        toast.show();
+//        Toast.makeText(getContext(), text, Toast.LENGTH_SHORT).show();
     }
 
     public static void showToast(@StringRes int resId){
-        Toast.makeText(getContext(), resId, Toast.LENGTH_SHORT).show();
+        Toast toast = getToast();
+        toast.setText(resId);
+        toast.setDuration(Toast.LENGTH_SHORT);
+        toast.show();
+//        Toast.makeText(getContext(), resId, Toast.LENGTH_SHORT).show();
     }
 
     public static void showSnackBar(CharSequence text, View view){
@@ -23,5 +31,14 @@ public class ToastUtil extends ApplicationUtil{
 
     public static void showSnackBar(@StringRes int resId, View view){
         Snackbar.make(view, resId, Snackbar.LENGTH_SHORT).show();
+    }
+
+    private static Toast mToast;
+
+    public static Toast getToast(){
+        if(mToast == null){
+            mToast = Toast.makeText(getContext(), 0, Toast.LENGTH_SHORT);
+        }
+        return mToast;
     }
 }
